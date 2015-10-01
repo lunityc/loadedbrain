0a2dfed4892dc4717ddbd08f28e88c6a1afdf08f
lunityc <lunityc@gmail.com>
2015-10-01 16:07:18 -0700

    First Version - Display Time

---
+++ UnityRemoteTest/Assets/script/ShowTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowTime : MonoBehaviour 
+{
+	Text showTimeText;
+
+	// initialization
+	void Start () 
+	{
+		showTimeText = GetComponent<Text>();
+
+		string theTime = System.DateTime.Now.ToString("hh:mm:ss"); 
+		string theDate = System.DateTime.Now.ToString("MM/dd/yyyy"); 
+
+		Debug.Log( "Time: " + theTime);
+		Debug.Log( "Date: " + theDate);
+	}
+	
+	// called once per frame
+	void Update () 
+	{
+		string theTime = System.DateTime.Now.ToString("hh:mm:ss"); 
+		string theDate = System.DateTime.Now.ToString("MM/dd/yyyy"); 
+		showTimeText.text = theDate + " \n " + theTime;
+	}
+}
